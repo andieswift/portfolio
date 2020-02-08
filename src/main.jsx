03633a0a4a390c6171b34639fa03a201aedd09a1
Andie Swift <andie.swift5@gmail.com>
2020-02-07 16:48:46 -0800

    added drawing a box animation

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+export default function Main (){
+  return (
+  <div className="top-image h-100">
+    <div className="top-right"></div>
+    <div className="bottom-left"></div>
+  </div>
+  );
+}
